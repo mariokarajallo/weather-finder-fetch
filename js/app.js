632c755126d81cc2a88b6cab11136cfe5bd26f08
mariokarajallo <mariokarajallo92@gmail.com>
2023-04-24 23:42:23 -0400

    feat: adds selectors

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// selectores
+const container = document.querySelector(".container");
+const resultado = document.querySelector("#resultado");
+const formulario = document.querySelector("#formulario");
